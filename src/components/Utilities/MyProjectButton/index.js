fd88be7df9db95681211c2c8ac543bc906b598d5
ryaand <bahriansyahherlambang3@gmail.com>
2026-05-17 14:48:15 +0700

    fix: disable image and link dragging

--- src/components/Utilities/MyProjectButton/index.js
+++ src/components/Utilities/MyProjectButton/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const MyProjectButton = () => {
     return (
         <div>
-            <Link className="bg-gradient-to-r from-3/80 to-3/55 backdrop-blur-2xl p-2.5 px-4 rounded-full text-size- text-5" href="/MyProject" >
+            <Link draggable={false} className="bg-gradient-to-r from-3/80 to-3/55 backdrop-blur-2xl p-2.5 px-4 rounded-full text-size- text-5" href="/MyProject" >
                 My Project
             </Link>
         </div>
--- src/components/Utilities/Navbar/index.js
+++ src/components/Utilities/Navbar/index.js
@@ -13,23 +13,23 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="bg-white/10 rounded-[15] flex justify-between py-4 px-10 sm:px-5 items-center backdrop-blur-xs fixed text-2 font-medium shadow-xl my-8 inset-x-0 mx-auto w-[90%] sm:w-150 z-0">
+            <nav className="bg-white/10 select-none rounded-[15] flex justify-between py-4 px-10 sm:px-5 items-center backdrop-blur-xs fixed text-2 font-medium shadow-xl my-8 inset-x-0 mx-auto w-[90%] sm:w-150 z-0">
                 <Link className="flex font-bold text-lg text-2 gap-x-2" href="/">
-                    <Image src={logo} width={20} alt="logo"></Image>
+                    <Image draggable={false} className="select-none" src={logo} width={20} alt="logo"></Image>
                     RianArchive
                 </Link>
 
                 <div className="gap-5 hidden sm:flex text-sm">
-                    <Link href="/">Home</Link>
-                    <Link href="/about">About</Link>
-                    <Link href="/contact">Contact</Link>
+                    <Link draggable={false} href="/">Home</Link>
+                    <Link draggable={false} href="/about">About</Link>
+                    <Link draggable={false} href="/contact">Contact</Link>
                     <MyProjectButton></MyProjectButton>
                 </div>
 
                 <div onClick={() => setToggleNavbar(toggleNavbar ? false : true)} className="sm:hidden">
-                    <Image src={humberger} width={25
+                    <Image draggable={false} src={humberger} width={25
 
-                    } alt="negro`">
+                    } alt="humberger">
 
                     </Image>
                 </div>
@@ -38,20 +38,20 @@ const Navbar = () => {
             </nav>
 
             <div className={toggleNavbar ? "block" : "hidden"} >
-                <div className="fixed bg-white w-full h-full sm:hidden flex flex-col items-center">
+                <div className="fixed select-none bg-white w-full h-full sm:hidden flex flex-col items-center">
                     <div className="flex items-center w-full py-12 justify-between px-[13%]">
-                        <Link className="flex font-bold text-lg text-2 gap-x-2" href="/">
-                            <Image src={logo} width={20} alt="logo"></Image>
+                        <Link draggable={false} className="flex font-bold text-lg text-2 gap-x-2" href="/">
+                            <Image draggable={false} src={logo} width={20} alt="logo"></Image>
                             RianArchive
                         </Link>
                         <div onClick={() => setToggleNavbar(toggleNavbar ? false : true)}>
-                            <Image src={humberger_active} width={25} alt="memek"></Image>
+                            <Image draggable={false} src={humberger_active} width={25} alt="memek"></Image>
                         </div>
                     </div>
                     <div className="flex-col divide-y flex w-full px-[12%]">
-                        <Link className="py-3 pl-2 first:pt-0" href="/about">About</Link>
-                        <Link className="py-3 pl-2" href="/">Home</Link>
-                        <Link className="py-3 pl-2" href="/contact">Contact</Link>
+                        <Link draggable={false} className="py-3 pl-2 first:pt-0" href="/about">About</Link>
+                        <Link draggable={false} className="py-3 pl-2" href="/">Home</Link>
+                        <Link draggable={false} className="py-3 pl-2" href="/contact">Contact</Link>
                         <div className="py-3 pt-5 pl-1"><MyProjectButton></MyProjectButton></div>
                         
                     </div>
