e3393b2e2382081c0af32432d297f77af8974aa3
ryaand <bahriansyahherlambang3@gmail.com>
2026-05-17 14:57:14 +0700

    style: add MyProjectButton hover animation

--- src/components/Utilities/MyProjectButton/index.js
+++ src/components/Utilities/MyProjectButton/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const MyProjectButton = () => {
     return (
         <div>
-            <Link draggable={false} className="bg-gradient-to-r from-3/80 to-3/55 backdrop-blur-2xl p-2.5 px-4 rounded-full text-size- text-5" href="/MyProject" >
+            <Link draggable={false} className="bg-linear-to-r from-3/80 to-3/55 backdrop-blur-2xl p-2.5 px-4 rounded-full text-5 hover:from-3/55 hover:to-3/80 duration-400 transition" href="/MyProject" >
                 My Project
             </Link>
         </div>
