6bd263c222eec2cc2bcc7a48f5a4b215ac9124cf
ryaand <bahriansyahherlambang3@gmail.com>
2026-05-20 01:07:32 +0700

    fix: solve background responsiveness issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
+import Background from "@/components/Utilities/background";
 
 export default function Home() {
   return (
-    <section className="bg-[url('@/public/Images/bg.svg')] w-full bg-center h-940.5 flex-col justify-center overflow-x-hidden">
+    <section className="bg-[url('@/public/Images/bg.svg')] w-full bg-center h-940.5 bg-cover flex-col justify-center overflow-x-hidden">
+      {/* bg-[url('@/public/Images/bg.svg')] */}
+
       <HeroSection></HeroSection>
+
     </section>
   );
 }
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -11,7 +11,7 @@ const HeroSection = () => {
             </div>
             <div className='w-full md:w-1/2 md:items-baseline flex-col flex items-center gap-1.5'>
                 <div className='flex gap-1 bg-linear-to-r from-3/50 to-3/35 py-0.5 px-2.5 rounded-full text-5 text-[10px] md:text-[15px] font-extralight'>
-                    <Image draggable={false} className='pointer-events-none' alt='' width={15} src={logo}></Image>
+                    <Image draggable={false} className='pointer-events-none md:w-3.75' alt='' width={10} src={logo}></Image>
                     Website Development
                 </div>
                 <p className='text-[30px]/8 text-3 font-bold text-center md:text-left md:text-[35px] lg:text-[55px]/13'>Hello, <a className='text-2'>I'am Rian</a>,<br></br>Front-End Developer<br></br>Based in Indonesia.</p>
