eb568e6e4d7106657681691509da67b3c50ed71c
ryaand <bahriansyahherlambang3@gmail.com>
2026-05-27 21:11:30 +0700

    fix: disable draggable image on FocusingSection

--- src/components/FocusingSection/index.js
+++ src/components/FocusingSection/index.js
@@ -8,7 +8,7 @@ const FocusingSection = () => {
                 Focusing
             </div>
             <div className="flex flex-col items-center md:flex-row md:justify-between ">
-                <Image src={furina2} className="w-70 py-10 lg:w-90 -rotate-15" alt=""></Image>
+                <Image src={furina2} draggable={false} className="w-70 py-10 lg:w-90 -rotate-15 pointer-events-none" alt=""></Image>
                 <p className="px-15 text-center lg:text-xl md:text-right">
                     Fokus utama saya adalah membangun arsitektur front-end yang skalabel menggunakan ekosistem Next.js dan Tailwind CSS. Saya memastikan setiap baris kode yang ditulis tidak hanya menghasilkan visual yang presisi, tetapi juga mengutamakan optimasi kecepatan akses, skor Core Web Vitals yang tinggi, dan struktur yang ramah SEO. Bagi saya, performa adalah fondasi dari pengalaman pengguna yang superior.
                 </p>
