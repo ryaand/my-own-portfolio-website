87943d291e103c4a70695577dec03bc1f1626d22
ryaand <bahriansyahherlambang3@gmail.com>
2026-06-14 21:47:05 +0700

    fix: disable draggale image in specialties section

--- src/components/SpecialtiesSection/index.js
+++ src/components/SpecialtiesSection/index.js
@@ -12,18 +12,18 @@ const SpecialtiesSection = () => {
             </div>
             <div className="flex flex-col items-center pt-10 gap-7 text-primary lg:flex-row lg:justify-center w-full">
                 <DefaultBoxContainer>
-                    <div className="flex flex-col gap-3 pt-4 pl-5">
-                        <Image src={FrontEnd} alt="" className="w-10"></Image>
+                    <div className="flex flex-col gap-3 pt-4 pl-5 w-90 h-65">
+                        <Image src={FrontEnd} alt="" width={30} className="pointer-events-none"></Image>
                         <div className="whitespace-pre-line font-black  text-2xl">
                             Front-End <br /> Development
                         </div>
                         <div className="font-extralight">
-                            Memiliki kompetensi mendalam<br />dalam pengembangan front-end<br /> web.
+                            Memiliki kompetensi mendalam<br/>dalam pengembangan front-end<br/> web.
                         </div>
                     </div>
                 </DefaultBoxContainer>
                 <DefaultBoxContainer>
-                    <div className="flex flex-col gap-4 pl-5 pt-7">
+                    <div className="flex flex-col gap-4 pl-5 pt-7 w-90 h-65">
                         <Image src={UiUx} alt="" width={30} draggable={false} className="pointer-events-none"></Image>
                         <div className="whitespace-pre-line font-black  text-2xl">
                             UI/UX Designer
