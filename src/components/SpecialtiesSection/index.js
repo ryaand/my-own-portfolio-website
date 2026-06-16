af71f7a9dd6f6ffe05f711cd84ce518283dee3c1
ryaand <bahriansyahherlambang3@gmail.com>
2026-06-16 22:38:01 +0700

    fix: remove all padding from DefaultBoxContainer and styling in each section

--- src/components/SpecialtiesSection/index.js
+++ src/components/SpecialtiesSection/index.js
@@ -6,33 +6,39 @@ import UiUx from "@/public/Images/layer.svg"
 const SpecialtiesSection = () => {
     return (
         <div className="pt-10">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center pb-5">
                 <div className="font-bold text-primary text-xl">My Spescialties</div>
                 <div className="font-extralight text-gray-500">I am competent at</div>
             </div>
-            <div className="flex flex-col items-center pt-10 gap-7 text-primary lg:flex-row lg:justify-center w-full">
-                <DefaultBoxContainer>
-                    <div className="flex flex-col gap-3 pt-4 pl-5 w-90 h-65">
-                        <Image src={FrontEnd} alt="" width={30} className="pointer-events-none"></Image>
-                        <div className="whitespace-pre-line font-black  text-2xl">
-                            Front-End <br /> Development
+            <div className="flex flex-col items-center gap-7 text-primary lg:flex-row lg:justify-center">
+                <div className="flex">
+                    <DefaultBoxContainer>
+                        <div className="flex flex-col justify-center p-10 gap-2 w-85 h-60">
+                            <div>
+                                <Image src={FrontEnd} alt="" width={30} className="pointer-events-none"></Image>
+                            </div>
+                            <div className="whitespace-pre-line font-black  text-2xl">
+                                Front-End <br /> Development
+                            </div>
+                            <div className="font-extralight">
+                                Memiliki kompetensi mendalam<br />dalam pengembangan front-end<br /> web.
+                            </div>
                         </div>
-                        <div className="font-extralight">
-                            Memiliki kompetensi mendalam<br/>dalam pengembangan front-end<br/> web.
+                    </DefaultBoxContainer>
+                </div>
+                <div className="flex">
+                    <DefaultBoxContainer>
+                        <div className="flex flex-col justify-center p-10 gap-2 w-85 h-60">
+                            <Image src={UiUx} alt="" width={30} draggable={false} className="pointer-events-none"></Image>
+                            <div className="whitespace-pre-line font-black  text-2xl">
+                                UI/UX Designer
+                            </div>
+                            <div className="font-extralight">
+                                Memiliki kompetensi mendalam<br />dalam pengembangan front-end<br /> web.
+                            </div>
                         </div>
-                    </div>
-                </DefaultBoxContainer>
-                <DefaultBoxContainer>
-                    <div className="flex flex-col gap-4 pl-5 pt-7 w-90 h-65">
-                        <Image src={UiUx} alt="" width={30} draggable={false} className="pointer-events-none"></Image>
-                        <div className="whitespace-pre-line font-black  text-2xl">
-                            UI/UX Designer
-                        </div>
-                        <div className="font-extralight">
-                            Memiliki kompetensi mendalam<br />dalam pengembangan front-end<br /> web.
-                        </div>
-                    </div>
-                </DefaultBoxContainer>
+                    </DefaultBoxContainer>
+                </div>
             </div>
         </div>
     )
--- src/components/ToolsAnfLangSection/index.js
+++ src/components/ToolsAnfLangSection/index.js
@@ -6,13 +6,13 @@ const ToolsAndLang = () => {
     </script>
     return (
         <div className="pt-10">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center pb-5">
                 <div className="font-bold text-primary text-xl">Tools and Languages</div>
                 <div className="font-extralight text-gray-500">I am using</div>
             </div>
-            <div className="w-full flex justify-center zoom-110 pt-3">
+            <div className="w-full flex justify-center zoom-110">
                 <DefaultBoxContainer>
-                    <div className="grid grid-cols-5 items-center justify-center gap-x-5 pb-4 sm:flex sm:gap-x-2 md:gap-4">
+                    <div className="grid grid-cols-5 items-center justify-center gap-x-5 p-4 sm:flex sm:gap-x-2 md:gap-4">
                             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 48 48">
                                 <path fill="#29b6f6" d="M44,11.11v25.78c0,1.27-0.79,2.4-1.98,2.82l-8.82,4.14L34,33V15L33.2,4.15l8.82,4.14 C43.21,8.71,44,9.84,44,11.11z"></path><path fill="#0277bd" d="M9,33.896L34,15V5.353c0-1.198-1.482-1.758-2.275-0.86L4.658,29.239 c-0.9,0.83-0.849,2.267,0.107,3.032c0,0,1.324,1.232,1.803,1.574C7.304,34.37,8.271,34.43,9,33.896z"></path><path fill="#0288d1" d="M9,14.104L34,33v9.647c0,1.198-1.482,1.758-2.275,0.86L4.658,18.761 c-0.9-0.83-0.849-2.267,0.107-3.032c0,0,1.324-1.232,1.803-1.574C7.304,13.63,8.271,13.57,9,14.104z"></path>
                             </svg>
