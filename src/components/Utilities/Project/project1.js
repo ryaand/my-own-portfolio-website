060ab1d2a1c53062d00b36283367f2ff66b71f39
ryaand <bahriansyahherlambang3@gmail.com>
2026-06-16 22:46:10 +0700

    style: restyle width in project1, project2, project3

--- src/components/Utilities/Project/project1.js
+++ src/components/Utilities/Project/project1.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Project1 = () => {
     return (
-        <div className="w-90 h-65 bg-[#d9d9d9]/10 bg-linear-to-br from-white/35 via-white/10 rounded-3xl shadow-xl backdrop-blur-2xl pt-4 px-4 flex-col justify-center -z-10 hover:scale-101 transform duration-250 ease-in-out">
+        <div className="w-85 h-65 bg-[#d9d9d9]/10 bg-linear-to-br from-white/35 via-white/10 rounded-3xl shadow-xl backdrop-blur-2xl pt-4 px-4 flex-col justify-center -z-10 hover:scale-101 transform duration-250 ease-in-out">
             <Link href="https://drive.google.com/file/d/1lqOqszyivCCP-Xx_yUzXEtn_Hol0MsLK/view?usp=sharing">
                 <Image src={uiProject} alt="" className="w-full object-cover object-top h-[70%] rounded-2xl">
                 </Image>
--- src/components/Utilities/Project/project2.js
+++ src/components/Utilities/Project/project2.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Project2 = () => {
     return (
 
-        <div className="w-90 h-65 bg-[#d9d9d9]/10 bg-linear-to-br from-white/35 via-white/10 rounded-3xl shadow-xl backdrop-blur-2xl pt-4 px-4 flex-col justify-center -z-10 hover:scale-101 transform duration-250 ease-in-out">
+        <div className="w-85 h-65 bg-[#d9d9d9]/10 bg-linear-to-br from-white/35 via-white/10 rounded-3xl shadow-xl backdrop-blur-2xl pt-4 px-4 flex-col justify-center -z-10 hover:scale-101 transform duration-250 ease-in-out">
             <Link href="https://josura-etp4w2wuh-josuraas-projects.vercel.app/">
                 <Image src={landingPageProject} alt="" className="w-full object-cover object-top h-[70%] rounded-2xl">
                 </Image>
